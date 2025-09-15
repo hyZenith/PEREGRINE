7001df46627782356070727f5d891be998bf85a5
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2025-09-15 20:34:14 +0530

    add post controller

---
+++ controllers/postController.js
@@ -0,0 +1,40 @@
+const Post = require("../models/Post.js")
+
+const getPosts = async(req, res) => {
+    const posts = await Post.find().sort({createdAt: -1});
+    res.json(posts);
+}
+
+const likedPost = async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({message: "post not found"})
+
+    post.likes += 1;
+    await post.save();
+    res.json({message:"post liked"})
+}
+
+
+const commentPost = async(req, res) => {
+    const {username, comment} = req.body;
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({message: "Post not found"})
+    
+
+    post.comments.push({username, comment});
+    await post.save();
+    res.json({message: "comment added"})
+}
+
+
+const sharePost = async (req,res) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({message: "Post not found"})
+    
+    post.shares += 1;
+    await post.save()
+    res.json({message: "post shared"})
+
+}
+
+module.exports = { getPost, likedPost, commentPost, sharePost };
