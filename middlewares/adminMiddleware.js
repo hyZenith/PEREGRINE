24b9b17ca6b603ee57465b0a2616b61708505705
zen_.ith <124890239+hyZenith@users.noreply.github.com>
2025-09-23 23:12:16 +0530

    some changes

--- middlewares/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ message: "Access denied. Admin only." });
-  }
-  next();
-};
-
-module.exports = isAdmin;
--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const register = require('../controllers/authController').registerUser;
-const login = require('../controllers/authController').loginUser;
-const router = express.Router();
-
-router.post('/login', login);
-router.post('/register', register);
-
-module.exports = router;
